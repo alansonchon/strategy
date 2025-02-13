1af2b963f89b16d676316cce56b37899c5fcaca2
alansonchon <alansonchon@gmail.com>
2025-02-13 15:20:28 +0800

    Modify fast.cpp

--- fast.cpp
+++ fast.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Group poor maintain.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
